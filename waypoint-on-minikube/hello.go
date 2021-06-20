720244536fe688057587c67effcd42650e3eb46d
Nuno Adrego <nuno.adrego@talkdesk.com>
2021-06-20 23:17:04 +0100

    Add Startup Log to hello.go

--- waypoint-on-minikube/hello.go
+++ waypoint-on-minikube/hello.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	log.Printf("Starting Hello Waypoint App.")
 	http.HandleFunc("/", HelloServer)
 	http.ListenAndServe(":8080", nil)
 }
